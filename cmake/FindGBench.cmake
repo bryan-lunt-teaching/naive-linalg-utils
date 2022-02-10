4f523fce0fae7c74e4acd5bfd090d779186cc7bd
Bryan Lunt <bjlunt2@illinois.edu>
2022-02-10 13:19:55 -0600

    Further improvements to FindGBench.cmake

--- cmake/FindGBench.cmake
+++ cmake/FindGBench.cmake
@@ -12,19 +12,16 @@
 #See the License for the specific language governing permissions and
 #limitations under the License.
 
-### Version 3 of this cmake file ###
+### Version 4 of this cmake file ###
 
 find_path(GBENCH_INCLUDE_DIR "benchmark/benchmark.h"
 	HINTS ${GBENCH_ROOT}/include
 	$ENV{GBENCH_ROOT}/include
 )
+mark_as_advanced(GBENCH_INCLUDE_DIR)
 
-#find_library(GBENCH_CORE_LIBRARIES_STATIC "benchmark")
-#find_library(GBENCH_MAIN_LIBRARIES_STATIC "benchmark_main")
-
-
-find_library(GBENCH_CORE_LIBRARIES_STATIC "${CMAKE_STATIC_LIBRARY_PREFIX}benchmark${CMAKE_STATIC_LIBRARY_SUFFIX}")
-find_library(GBENCH_MAIN_LIBRARIES_STATIC "${CMAKE_STATIC_LIBRARY_PREFIX}benchmark_main${CMAKE_STATIC_LIBRARY_SUFFIX}")
+find_library(GBENCH_LIBRARY "${CMAKE_STATIC_LIBRARY_PREFIX}benchmark${CMAKE_STATIC_LIBRARY_SUFFIX}")
+find_library(GBENCH_MAIN_LIBRARY "${CMAKE_STATIC_LIBRARY_PREFIX}benchmark_main${CMAKE_STATIC_LIBRARY_SUFFIX}")
 
 #find_library(GBENCH_CORE_LIBRARIES_SHARED "${CMAKE_SHARED_LIBRARY_PREFIX}benchmark${CMAKE_SHARED_LIBRARY_SUFFIX}")
 #find_library(GBENCH_MAIN_LIBRARIES_SHARED "${CMAKE_SHARED_LIBRARY_PREFIX}benchmark_main${CMAKE_SHARED_LIBRARY_SUFFIX}")
@@ -38,29 +35,40 @@ find_library(GBENCH_MAIN_LIBRARIES_STATIC "${CMAKE_STATIC_LIBRARY_PREFIX}benchma
 #follow that precedent.
 #
 
-if(GBENCH_CORE_LIBRARIES_STATIC AND GBENCH_MAIN_LIBRARIES_STATIC AND GBENCH_INCLUDE_DIR)
-	set(GBENCH_FOUND "TRUE")
-	set(GBENCH_BOTH_LIBRARIES ${GBENCH_CORE_LIBRARIES_STATIC} ${GBENCH_MAIN_LIBRARIES_STATIC})
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(GBench DEFAULT_MSG GBENCH_LIBRARY GBENCH_INCLUDE_DIR GBENCH_MAIN_LIBRARY)
+
+
+if(GBENCH_FOUND)
+	#set(GBENCH_FOUND "TRUE")
+	set(GBENCH_BOTH_LIBRARIES ${GBENCH_LIBRARY} ${GBENCH_MAIN_LIBRARY})
 	set(GBENCH_INCLUDE_DIRS ${GBENCH_INCLUDE_DIR})
 	#message("GBENCH: ${GBENCH_LIBRARY} ${GBENCH_LIBRARY_MAIN}")
 	
-	add_library(GBench::gbench_main STATIC IMPORTED )
-	set_target_properties(GBench::gbench_main PROPERTIES IMPORTED_LOCATION ${GBENCH_MAIN_LIBRARIES_STATIC})
-	target_link_libraries(GBench::gbench_main INTERFACE ${GBENCH_MAIN_LIBRARIES_STATIC})
-	target_include_directories(GBench::gbench_main INTERFACE ${GBENCH_INCLUDE_DIRS})
+	find_package(Threads QUIET REQUIRED)
 	
-	add_library(GBench::gbench STATIC IMPORTED )
-	set_target_properties(GBench::gbench PROPERTIES IMPORTED_LOCATION ${GBENCH_CORE_LIBRARIES_STATIC})
-	target_link_libraries(GBench::gbench INTERFACE ${GBENCH_CORE_LIBRARIES_STATIC})
-	target_include_directories(GBench::gbench INTERFACE ${GBENCH_INCLUDE_DIRS})
+	if(NOT TARGET GBench::gbench)
+		add_library(GBench::gbench STATIC IMPORTED )
+		set_target_properties(GBench::gbench PROPERTIES IMPORTED_LOCATION ${GBENCH_LIBRARY})
+		target_link_libraries(GBench::gbench INTERFACE ${GBENCH_LIBRARY})
+		target_include_directories(GBench::gbench INTERFACE ${GBENCH_INCLUDE_DIRS})
+		
+		if(TARGET Threads::Threads)
+			target_link_libraries(GBench::gbench INTERFACE Threads::Threads)
+		endif()
+	endif()
+	
+	if(NOT TARGET GBench::gbench_main)
+		add_library(GBench::gbench_main STATIC IMPORTED )
+		set_target_properties(GBench::gbench_main PROPERTIES IMPORTED_LOCATION ${GBENCH_MAIN_LIBRARY})
+		target_link_libraries(GBench::gbench_main INTERFACE GBench::gbench)
+		target_link_libraries(GBench::gbench_main INTERFACE ${GBENCH_MAIN_LIBRARY})
+		target_include_directories(GBench::gbench_main INTERFACE ${GBENCH_INCLUDE_DIRS})
+	endif()
 	
 	#add_library(GoogleBenchmark INTERFACE)
 	#target_link_libraries(GoogleBenchmark INTERFACE GBench::gbench)
 	#TODO: Make linking (and even finding) main optional.
 	#target_link_libraries(GoogleBenchmark INTERFACE GBench::gbench_main)
 	
-	
-	message("GBENCH: Found! ${GBENCH_BOTH_LIBRARIES} ${GBENCH_INCLUDE_DIRS}")
-else()
-	message("GBENCH: Not Found")
 endif()
